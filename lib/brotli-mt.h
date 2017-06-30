5b97e7020136ffb53e839b9f176c60ade43eeb00
Tino Reichardt <milky-7zip@mcmilk.de>
2017-06-30 19:15:15 +0200

    Added various fixes from zstmt of 7-Zip ZS

--- lib/brotli-mt.h
+++ lib/brotli-mt.h
@@ -32,6 +32,9 @@ extern "C" {
 #define BROTLIMT_MAGICNUMBER     0x5242U /* BR */
 #define BROTLIMT_MAGIC_SKIPPABLE 0x184D2A50U
 
+#define BROTLI_VERSION_MAJOR 0
+#define BROTLI_VERSION_MINOR 6
+
 /* **************************************
  * Error Handling
  ****************************************/
