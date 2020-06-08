38bac1397ababdf0ab43599a224906a03b3b9ea0
Tino Reichardt <milky-7zip@mcmilk.de>
2020-06-08 19:15:14 +0200

    Change Github workflow test, move it mainly to Makefile

--- programs/snappy-mt.c
+++ programs/snappy-mt.c
@@ -1,5 +1,11 @@
 
 /**
+ * Copyright (c) 2020 @jinfeihan57
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 
 #include "snappy-mt.h"
